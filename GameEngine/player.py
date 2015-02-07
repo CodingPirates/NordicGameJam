952e0a798ca2441697e35cf4840be27f729278c9
Emile <blemille@hotmail.com>
2015-02-07 23:15:08 +0100

    player script update

--- GameEngine/player.py
+++ GameEngine/player.py
@@ -3,14 +3,13 @@ import pygame, sys, Element
 class Player (Element):
 
 	# The player controls, no keyboard handling is done n this class all
-	movementSpeed = 2
-	jumpHeight    = 4
+	movementAcc = 2
+	jumpHeight = 4
+	movementSpeed = 0
+	maxSpeed = 5
+	control = 0
 
 	def anim (deltaTime):
-
-		changeTimeTop = movementSpeed * 100
-
-		changeTime += deltaTime
 
 		animRightImages [0] = pygame.image.load ('/Graphics/Player/anim/right_walk/player_right_walk01.png')
 		animRightImages [1] = pygame.image.load ('/Graphics/Player/anim/right_walk/player_right_walk02.png')
@@ -19,32 +18,15 @@ class Player (Element):
 		animLeftImages [0] = pygame.image.load ('/Graphics/Player/anim/left_walk/player_left_walk01.png')
 		animLeftImages [1] = pygame.image.load ('/Graphics/Player/anim/left_walk/player_left_walk02.png')
 		animLeftImages [2] = pygame.image.load ('/Graphics/Player/anim/left_walk/player_left_walk03.png')
+		
+	def updatePos(deltaTime):
+		movementSpeed += movementAcc * deltaTime * control
+		if movementSpeed > maxSpeed:
+			movementSpeed = 5
+		elif movementSpeed < -maxSpeed:
+		    movementSpeed = -5
+		setPosition(getPostion()[0]+movementSpeed, getPostion()[1])
 
-		i ++
-
-		if (changeTime >= changeTimeTop):
-
-			if (movementSpeed < 0):
-
-				screen.blit (animLeftImages [i], (x,y))
-
-			elif (movementSpeed < 0):
-
-				screen.blit (animRightImages [i], (x,y))
-
-
-	def moveRight ():
-
-	    pos = getPostion ()
-
-	    pos[0] += movementSpeed
-
-	    setPostion(pos)
-
-	def moveLeft ():
-
-	    pos = getPostion ()
-
-	    pos[0] -= movementSpeed
-
-	    setPostion ()
+	def tick(deltaTime, keyEvent=False):
+	    self.anim(deltaTime)
+		self.updatePos(deltaTime)
