9bd70a11c69dea4fcb7f0c992e9c5b2b307faca5
Per Hansen <nallic@users.noreply.github.com>
2015-02-07 17:02:54 +0100

    initial Element class

---
+++ GameEngine/Element.py
@@ -0,0 +1,25 @@
+class Element:
+	# get get lower left pixel position
+	def getPosition():
+	
+	# get box surrounding element (the size of element)
+	def getBoundingBox():
+
+	# get the current Image for this element
+	def getSurface():
+
+	# get depth in image
+	#  0: Background
+	#  1: Background elements
+	# 10: Enemies
+	# 11: Player
+	# 12: Foreground elements 
+	def getLayer():
+
+	# update all element data (delta time is in milliseconds)
+	def tick(deltaTime):
+
+	# take player controller events
+	def control(keyEvent):
+
+	
