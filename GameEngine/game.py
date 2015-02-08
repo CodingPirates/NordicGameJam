d7ffe616de15169c3e0794856c2afe5b243fae58
Niels Horn <Niels@Programmer.net>
2015-02-08 11:15:04 +0100

    Added quit on 'X'

--- GameEngine/game.py
+++ GameEngine/game.py
@@ -90,6 +90,14 @@ class runGame:
             # Update double buffered screen
             pygame.display.flip()
 
+            for event in pygame.event.get ():
+
+                if event.type == QUIT:
+
+                    pygame.quit ()
+
+                    sys.exit () 
+
 game = runGame()
 
 game.start()
