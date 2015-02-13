706caf738bd1173b10b6900944f61ca6f6b67034
Emile <blemille@hotmail.com>
2015-02-13 20:52:30 +0100

    fixed loading AGAIN & added animations

--- GameEngine/player.py
+++ GameEngine/player.py
@@ -5,36 +5,54 @@ class Player (Element.Element):
     def __init__(self, position, size, image, layer):
 
         # The player controls, no keyboard handling is done n this class all
-        self.movementAcc = 2
-        self.jumpHeight = 4
-        self.movementSpeed = 0
-        self.maxSpeed = 5
+        self.movementAcc = 0.02
+        self.jumpHeight = 4.0
+        self.movementSpeed = 0.0
+        self.maxSpeed = 6.0
         self.control = 0
-        self.animMode = "still"
+        self.friction = 7.0
+        self.animMode = "none"
+        self.animClock = 0.0
 
         self.animRightImages = [pygame.image.load ('Graphics/Player/anim/right_walk/player_right_walk01.png').convert()]
         self.animRightImages.append(pygame.image.load ('Graphics/Player/anim/right_walk/player_right_walk02.png').convert())
         self.animRightImages.append(pygame.image.load ('Graphics/Player/anim/right_walk/player_right_walk03.png').convert())
-        self.animRightImages.append(pygame.image.load ('Graphics/Player/player_break01_right.png').convert())
-
+        
         self.animLeftImages = [pygame.image.load ('Graphics/Player/anim/left_walk/player_left_walk01.png').convert()]
         self.animLeftImages.append(pygame.image.load ('Graphics/Player/anim/left_walk/player_left_walk02.png').convert())
         self.animLeftImages.append(pygame.image.load ('Graphics/Player/anim/left_walk/player_left_walk03.png').convert())
-        self.animLeftImages.append(pygame.image.load ('Graphics/Player/player_break01_left.png').convert())
+
+        self.animBreakLeft = pygame.image.load ('Graphics/Player/player_break01_left.png').convert()
+        self.animBreakRight = pygame.image.load ('Graphics/Player/player_break01_right.png').convert()
+        #self.animStandingStill = pygame.ingame.load('Graphics/Player/player_standing.png').convert()
 
         super(Player, self).__init__(position, size, image, layer)
 
     def anim (self, deltaTime):
         if self.movementSpeed > 0:
             if self.control == -1:
-                self.animMode = "rightStop"
+                self.image = self.animBreakRight
+                self.animMode = "none"
             else:
                 self.animMode = "right"
         elif self.movementSpeed < 0:
             if self.control == 1:
-                self.animMode = "leftStop"
+                self.image = self.animBreakLeft
+                self.animMode = "none"
             else:
                 self.animMode = "left"
+        else:
+            self.animMode = "none"
+            #self.image = self.animStandingStill
+        
+        self.animClock += self.movementSpeed*deltaTime/200.0
+        self.animClock = self.animClock%3
+        if self.animMode == "right":
+            self.image = self.animRightImages[int(self.animClock)]
+        elif self.animMode == "left":
+            self.image = self.animLeftImages[int(self.animClock)]
+            
+            
 
     def updatePos(self, deltaTime):
         if self.movementSpeed < 0.1 and self.movementSpeed > -0.1:
@@ -49,6 +67,8 @@ class Player (Element.Element):
         
         print(self.movementSpeed)    
         self.setPosition((self.getPosition()[0]+self.movementSpeed, self.getPosition()[1]))
+
+        self.movementSpeed = self.movementSpeed - self.movementSpeed / self.friction
 
     def checkKeys (self, keyEvent):
 
